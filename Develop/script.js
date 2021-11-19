f0c4ba38a36f07704f08c7235d9b0e25a799b02a
Katie Churchwell (she/her) <87393465+katiechurchwell@users.noreply.github.com>
2021-11-19 15:41:36 -0800

    random lower, upper and number functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,29 @@
 // Assignment code here
+//Generators (from ASCII characters)
+//Lower case
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26)+97);
+}
+//Upper case
+function getRandomUpper(){
+  return String.fromCharCode(Math.floor(Math.random() * 26)+65);
+}
+//Numbers
+function getRandomNumber(){
+  return (Math.floor(Math.random() * 10));
+}
+
+console.log(getRandomNumber());
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// Write password to the #password textbox
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  
   passwordText.value = password;
 
 }
